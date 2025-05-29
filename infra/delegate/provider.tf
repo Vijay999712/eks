14e3b2b1e8ade090d8607fa5f782bf9eeee8e488
mandavikas17 <vikasmanda1249@gmail.com>
2025-05-29 10:55:22 +0530

    Update provider.tf

--- infra/delegate/provider.tf
+++ infra/delegate/provider.tf
@@ -15,9 +15,13 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
+    config_path = "~/.kube/config"
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
     load_config_file       = false
+    }
+}
+
   }
 }
