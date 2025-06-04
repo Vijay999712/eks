2f1a62306b3f0cebcdd9a1f7538c42c48dfd0ff9
Vijay999712 <vijaybabu920@gmail.com>
2025-06-04 15:35:19 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "vm-state-bucket-harness"
+    bucket         = "vj-state-bucket-harness"
     key            = "eks-cluster/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-lock-table"
